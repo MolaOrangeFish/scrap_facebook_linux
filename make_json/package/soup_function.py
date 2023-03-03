359df3d383a31a21c1df72af61048b65d374ac9a
MolaOrangeFish <kittichetadulroj@gmail.com>
2023-03-03 20:38:24 +0700

    classify comment and post

--- make_json/package/soup_function.py
+++ make_json/package/soup_function.py
@@ -15,28 +15,32 @@ def get_text_facebook(time,URL):
                 text = cleanning_except_emoji(post_text)
                 split = split_word(text)
                 print(f"Post_text  : {split}\n\n")
-                check_post = check_close_post(split) #will return not_found or found as str check the word "sold" in text list
+                check_post = check_close_post(split,"post") #will return not_found or found as str check the word "sold" in text list
 
                 comment_text = get_comment_facebooks(URL) ##Get comment of poster and check the comment that have word mean this post that closed
                 text = cleanning_except_emoji(comment_text)
                 split = split_word(text)
-                check_comment = check_close_post(split)
+                check_comment = check_close_post(split,"comment")
                 if check_post == "found" or check_comment == "found": ##if in post or comment found word that mean like this post is colsed
                     print(f"#################\nFOUND delete data {time}\n#################")
-                    remove_data_in_firebase(time)
+                    # remove_data_in_firebase(time)
         except Exception as e:
             try: #try to remove again if error will send error to firebase 
                     remove_data_in_firebase(time)
+
             except:
                 print(f"Error found::{e}")
                 send_error(str(e))
 
 
-def check_close_post(text_list):
+def check_close_post(text_list,data_type):
     #make all char. to lower form
     text_list = set_lower(text_list)
     found_check = "not_found"
-    bag_of_word = db.reference("close_post") #get the bag of word from firebase 
+    if(data_type=="post"):
+        bag_of_word = db.reference("close_post") #get the bag of word from firebase 
+    elif(data_type=="comment"):
+        bag_of_word = db.reference("close_post_comment") #get the bag of word from firebase 
     for word in set(bag_of_word.get()):
         if word in set(text_list):
             print(f"Close Found::{word}")
