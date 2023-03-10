8feae7842d7f955461c01299454577bcdc6f49a1
MolaOrangeFish <kittichetadulroj@gmail.com>
2023-03-10 22:15:06 +0700

    scrapdata

--- run.py
+++ run.py
@@ -34,6 +34,8 @@ def run_scrap():
 
 schedule.every().day.at("06:00").do(run_scrap)
 schedule.every().day.at("18:00").do(run_scrap)
+schedule.every().day.at("21:16").do(run_scrap)
+
 
 while True:
     schedule.run_pending()
